59c96bdd0f58cd1ea232c194088be6a246bf13c0
animewolf208 <animeserieswolf@gmail.com>
2025-05-08 10:44:02 +0530

    Add files via upload

---
+++ accountancy_ch1.js
@@ -0,0 +1,62 @@
+const accountancy_ch1 = [
+  {
+    chapter: "Introduction to Accounting",
+    question: "Accounting is a",
+    options: ["Art", "Science", "Both", "None"],
+    answer: "Both"
+  },
+  {
+    chapter: "Introduction to Accounting",
+    question: "Which is not a branch of accounting?",
+    options: ["Financial Accounting", "Management Accounting", "Cost Accounting", "Social Accounting"],
+    answer: "Social Accounting"
+  },
+  {
+    chapter: "Introduction to Accounting",
+    question: "The main object of accounting is to ascertain:",
+    options: ["Profit or loss", "Financial position", "Investment", "Turnover"],
+    answer: "Profit or loss"
+  },
+  {
+    chapter: "Introduction to Accounting",
+    question: "Accounting records only:",
+    options: ["Monetary transactions", "Non-monetary transactions", "All events", "Memorandum"],
+    answer: "Monetary transactions"
+  },
+  {
+    chapter: "Introduction to Accounting",
+    question: "Business transactions are recorded in:",
+    options: ["Chronological order", "Reverse order", "Any order", "None of the above"],
+    answer: "Chronological order"
+  },
+  {
+    chapter: "Introduction to Accounting",
+    question: "Book-keeping is the _______ step of accounting.",
+    options: ["First", "Second", "Third", "Last"],
+    answer: "First"
+  },
+  {
+    chapter: "Introduction to Accounting",
+    question: "Accounting begins with:",
+    options: ["Summarising", "Classifying", "Identifying", "Communication"],
+    answer: "Identifying"
+  },
+  {
+    chapter: "Introduction to Accounting",
+    question: "External users of accounting information are:",
+    options: ["Owners", "Managers", "Government", "Employees"],
+    answer: "Government"
+  },
+  {
+    chapter: "Introduction to Accounting",
+    question: "Recording of financial transactions is called:",
+    options: ["Book keeping", "Accounting", "Journal", "Ledger"],
+    answer: "Book keeping"
+  },
+  {
+    chapter: "Introduction to Accounting",
+    question: "Who is known as the father of accounting?",
+    options: ["Luca Pacioli", "Marshall", "Kohler", "Adam Smith"],
+    answer: "Luca Pacioli"
+  }
+];
